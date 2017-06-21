10d6b08366e83adc341eb41b8fbff348f25e1a68
vnframe <vnframe95@icloud.com>
2017-06-21 10:52:07 -0400

    ex 4

--- print_to_save.rb
+++ print_to_save.rb
@@ -5,14 +5,21 @@ x=0
 100.times do
 		 
 	x+=1
-	if x % 3 == 0
-	p "mined"
-	else
-	if x == 5
-	p "minds"
-       	else
-	p x
+		if x % 3 ==0 and x % 5 == 0
+		p "mined minds"
+ 		
+			else
+			if x % 3 == 0
+			p "mined"
+				
+				else
+				if x % 5 == 0
+				p "minds"
+				
+					else
+					p x
+				end
+			end
+		end
 	end
-	end
-end
 
