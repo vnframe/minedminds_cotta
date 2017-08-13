0f12f96dde20dbb0dd6da5f83dba82b2585cb0eb
vnframe <vnframe95@icloud.com>
2017-08-13 16:07:09 -0400

    comments

--- print_to_save.rb
+++ print_to_save.rb
@@ -1,25 +1,24 @@
 #!/usr/bin/env ruby
 
 print 'Hello World'
-x=0
-100.times do
-		 
-	x+=1
-		if x % 3 ==0 and x % 5 == 0
+x=0 #sets counter
+100.times do #loops 100 times
+
+	x+=1 #adds one to counter each loop
+		if x % 3 ==0 and x % 5 == 0 #if a number is divisible by both 3 and 5, it will be replaced with "mined minds"
 		p "mined minds"
- 		
+
 			else
-			if x % 3 == 0
+			if x % 3 == 0 #if a number is divisible by only 3, it will be replaced with "mined"
 			p "mined"
-				
+
 				else
-				if x % 5 == 0
+				if x % 5 == 0 #if a number is divisible only 5, it will be replaced with "minds"
 				p "minds"
-				
+
 					else
 					p x
 				end
 			end
 		end
 	end
-
